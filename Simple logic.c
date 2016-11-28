74da98cba641e1d148b3be9bef25df258fea2702
Hackamac <ragav.upreti1@gmail.com>
2016-11-28 10:45:38 +0545

    Add files via upload

---
+++ Simple logic.c
@@ -0,0 +1,37 @@
+
+/*
+Student Name: Ragav Upreti
+Subject : Programming fundamental
+Lab sheet No. : 7
+Program: WAP to enter the marks of 7 subject and produce result
+Date: Nov 28
+*/
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a;
+prinf("Enter a number: \n");
+scanf("%d",a);
+if (n/2==0)
+{
+    printf("Number is even: \n");
+}
+else
+{
+printf("number is odd \n");
+}
+if (n>0)
+{
+    printf("number is positive \n ");
+
+}
+else
+{
+    printf("the number is negative. \n");
+}
+    }
+return 0;
+getch();
+}
