8c603bd2edcc38cfd57c2b111b350b968c5b3261
Ragav Upreti <ragav.upreti1@gmail.com>
2016-11-30 12:23:56 +0545

    Add files via upload

---
+++ Assingment operators.c
@@ -0,0 +1,26 @@
+/*
+Student name : Ragav Upreti
+Subject: Programming Fundamental
+Roll NO:
+Program : WAP to use assignment operators
+Date: 30 Nov
+*/
+
+#include<stdio.h>
+#include<conio.h>
+
+int main ()
+{
+    int a,b;
+
+printf("enter the first number: \n");
+scanf("%d",&a);
+printf("Enter the second number: \n");
+scanf("%d",&b);
+a+=b;
+printf("+= %d",a);
+
+getch ();
+return 0;
+
+}
