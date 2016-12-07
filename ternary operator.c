f014b4e7b76ebc470b21c3c95cb17755253b44f4
Ragav Upreti <ragav.upreti1@gmail.com>
2016-12-07 11:44:29 +0545

    Add files via upload

---
+++ ternary operator.c
@@ -0,0 +1,23 @@
+/*
+Student Name: Ragav Upreti
+Subject: Programming Fundamental
+Roll No: 25
+Program: WAP to Check odd or even using ternary operator
+Date: 7 Dec
+*/
+
+#include<stdio.h>
+
+
+int main ()
+{
+	int a;
+	printf("Enter a number \n");
+	scanf("%d",&a);
+	
+	(a%2==0)? printf("%d is even",a) :printf("%d is odd",a);
+	
+	
+	
+return 0;	
+}
