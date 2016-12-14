444cd39ad14b1ae6d7b2b9d2edcbd6668d8e0c96
Ragav Upreti <ragav.upreti1@gmail.com>
2016-12-14 11:45:04 +0545

    Add files via upload

---
+++ 5 and 11 ternary.c
@@ -0,0 +1,47 @@
+/*
+
+StudentName: Ragav Upreti
+Subject:Programming Fundamentals.
+Rol No:25
+Lab No:13
+program:Write a make a simple calcultaor using switch operators 
+Date:Dec 14
+
+*/
+
+#include<stdio.h>
+
+
+int main()
+{
+	
+	intn,a,b,sum;
+	
+	printf("______________________________ \n);
+	printf("\t ***Simple Calculator*** \n");
+	printf(">>>>>> Choose any number \n")
+	printf("*1 for addition \n");
+	printf("*2 for subtraction \n");
+	printf("*3 for Division \n");
+	printf("*4 for multiplication \n";
+	scanf("%d",&n);
+	switch(n)
+	{
+		case 1:
+		printf("enter any two numbers \n");
+		sacnf("%d",&a,&b);
+		sum=a+b;
+		Printf("%d\n",sum);
+	}
+		
+			
+	
+	
+		
+		
+		
+		return 0;
+		}
+		
+		
+		
