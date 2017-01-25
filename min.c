d47e46a0e5f51a62eeeea8fa40abee58765a7eaf
Ragav Upreti <ragav.upreti1@gmail.com>
2017-01-25 10:47:41 +0545

    Add files via upload

---
+++ min.c
@@ -0,0 +1,44 @@
+/*
+ student name:Ragav Upreti
+ lab sheet :20
+ program:WAP to find area of circle using function
+ date:25 jan
+ Roll No:25
+ */
+ #include<stdio.h>
+
+ int check();
+ int main()
+ 
+ {
+ 
+ check();
+ 
+
+ 
+ 
+ return 0;
+ 
+
+  }
+  
+  int check ()
+  {
+	 int a,b;
+	 printf("Enter a number \n");
+	 scanf("%d",&a);
+	 
+	 printf("Enter another number \n");
+	 scanf("%d",&b);
+	 
+	 if (a>b)
+	 printf("%d is greater",a);
+	 
+	 else 
+	 printf("%d is greater ",b);
+	  
+	  return 0;
+  }
+  
+	 
+  
